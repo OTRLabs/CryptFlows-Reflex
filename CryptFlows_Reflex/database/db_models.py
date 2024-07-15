18873a99b723a8df68337c914621af9062825a42
cam <cammclain@skiff.com>
2024-07-15 01:31:15 -0500

    switch to use password hashing

--- CryptFlows_Reflex/database/db_models.py
+++ CryptFlows_Reflex/database/db_models.py
@@ -9,4 +9,5 @@ class User(rx.Model, table=True):
     id: int = Field(default=None, primary_key=True)
     username: str = Field(nullable=False)
     email: str = Field(nullable=False)
-    password: str = Field(nullable=False)
+    password_hash: str = Field(nullable=False)
+    
