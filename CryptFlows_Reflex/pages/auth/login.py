1b9c5d6a87b7420d18f689fb16adce7a05d85613
cam <cammclain@skiff.com>
2024-07-15 03:10:20 -0500

    set up auth pages

---
+++ CryptFlows_Reflex/pages/auth/login.py
@@ -0,0 +1,99 @@
+
+import reflex as rx
+
+from CryptFlows_Reflex.database.db_models import User
+
+class LoginState(rx.State):
+    email: str
+    password: str
+    
+    def login(self, email: str, password: str):
+        with rx.session() as session:
+            user = session.select().where(User.username.contains(User.email == self.email)).first()
+            if user and user.password == self.password:
+                return True
+            else:
+                return False
+            
+    
+
+def login_default_icons() -> rx.Component:
+    return rx.card(
+        rx.vstack(
+            rx.center(
+                rx.image(
+                    src="/logo.jpg",
+                    width="2.5em",
+                    height="auto",
+                    border_radius="25%",
+                ),
+                rx.heading(
+                    "Sign in to your account",
+                    size="6",
+                    as_="h2",
+                    text_align="center",
+                    width="100%",
+                ),
+                direction="column",
+                spacing="5",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.text(
+                    "Email address",
+                    size="3",
+                    weight="medium",
+                    text_align="left",
+                    width="100%",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("user")),
+                    placeholder="user@reflex.dev",
+                    type="email",
+                    size="3",
+                    width="100%",
+                ),
+                spacing="2",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.hstack(
+                    rx.text(
+                        "Password",
+                        size="3",
+                        weight="medium",
+                    ),
+                    rx.link(
+                        "Forgot password?",
+                        href="#",
+                        size="3",
+                    ),
+                    justify="between",
+                    width="100%",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("lock")),
+                    placeholder="Enter your password",
+                    type="password",
+                    size="3",
+                    width="100%",
+                ),
+                spacing="2",
+                width="100%",
+            ),
+            rx.button("Sign in", size="3", width="100%"),
+            rx.center(
+                rx.text("New here?", size="3"),
+                rx.link("Sign up", href="#", size="3"),
+                opacity="0.8",
+                spacing="2",
+                direction="row",
+                width="100%",
+            ),
+            spacing="6",
+            width="100%",
+        ),
+        max_width="28em",
+        size="4",
+        width="100%",
+    )
---
+++ CryptFlows_Reflex/pages/auth/sign_up.py
@@ -0,0 +1,112 @@
+
+
+import reflex as rx
+from CryptFlows_Reflex.database.db_models import User
+
+class SignUpState(rx.State):
+    email: str
+    password: str
+    
+    def add_user_to_db(self):
+        with rx.session() as session:
+            session.add(
+                User(
+                    email=self.email,
+                    password_hash=self.password
+                )
+            )
+            session.commit()
+    
+    def sign_up(self, email: str, password: str):
+        with rx.session() as session:
+            user = session.select().where(User.email == self.email).first()
+            if user:
+                return False
+            else:
+                self.add_user_to_db()
+                return True
+
+
+def signup_default_icons() -> rx.Component:
+    return rx.card(
+        rx.vstack(
+            rx.center(
+                rx.image(
+                    src="/logo.jpg",
+                    width="2.5em",
+                    height="auto",
+                    border_radius="25%",
+                ),
+                rx.heading(
+                    "Create an account",
+                    size="6",
+                    as_="h2",
+                    text_align="center",
+                    width="100%",
+                ),
+                direction="column",
+                spacing="5",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.text(
+                    "Email address",
+                    size="3",
+                    weight="medium",
+                    text_align="left",
+                    width="100%",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("user")),
+                    placeholder="user@reflex.dev",
+                    type="email",
+                    size="3",
+                    width="100%",
+                ),
+                justify="start",
+                spacing="2",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.text(
+                    "Password",
+                    size="3",
+                    weight="medium",
+                    text_align="left",
+                    width="100%",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("lock")),
+                    placeholder="Enter your password",
+                    type="password",
+                    size="3",
+                    width="100%",
+                ),
+                justify="start",
+                spacing="2",
+                width="100%",
+            ),
+            rx.box(
+                rx.checkbox(
+                    "Agree to Terms and Conditions",
+                    default_checked=True,
+                    spacing="2",
+                ),
+                width="100%",
+            ),
+            rx.button("Register", size="3", width="100%"),
+            rx.center(
+                rx.text("Already registered?", size="3"),
+                rx.link("Sign in", href="#", size="3"),
+                opacity="0.8",
+                spacing="2",
+                direction="row",
+                width="100%",
+            ),
+            spacing="6",
+            width="100%",
+        ),
+        max_width="28em",
+        size="4",
+        width="100%",
+    )
