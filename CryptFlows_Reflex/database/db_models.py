35f7836075b2fbed3a4fc4f5c91a8a905ee9c318
cam <cammclain@skiff.com>
2024-07-15 01:30:54 -0500

    init user table

---
+++ CryptFlows_Reflex/database/db_models.py
@@ -0,0 +1,12 @@
+
+
+from sqlmodel import Field
+
+import reflex as rx
+
+
+class User(rx.Model, table=True):
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(nullable=False)
+    email: str = Field(nullable=False)
+    password: str = Field(nullable=False)
