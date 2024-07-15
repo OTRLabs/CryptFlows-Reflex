e9f3f2b0d0cc73d852d3afb370bbf7e36fbe767e
cam <cammclain@skiff.com>
2024-07-15 01:29:07 -0500

    update state & import Optional type

--- CryptFlows_Reflex/CryptFlows_Reflex.py
+++ CryptFlows_Reflex/CryptFlows_Reflex.py
@@ -4,11 +4,26 @@
 from CryptFlows_Reflex.pages import *
 
 import reflex as rx
+from typing import Optional
 
 
 class State(rx.State):
-    """Define empty state to allow access to rx.State.router."""
+    user: Optional[User] = None
 
+    def logout(self):
+        """Log out a user."""
+        self.reset()
+        return rx.redirect("/")
+
+    def check_login(self):
+        """Check if a user is logged in."""
+        if not self.logged_in:
+            return rx.redirect("/login")
+
+    @rx.var
+    def logged_in(self):
+        """Check if a user is logged in."""
+        return self.user is not None
 
 # Create the app.
 app = rx.App()
